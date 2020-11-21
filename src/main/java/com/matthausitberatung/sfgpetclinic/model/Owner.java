ed77b5203fc72835f3c9d99e2c6dec0effaeb81a
Maciej Pietruszewski <mac.pietruszewski@gmail.com>
2020-11-21 22:13:03 +0100

    Add POJOs. Closes #2

---
+++ src/main/java/com/matthausitberatung/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.matthausitberatung.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/matthausitberatung/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.matthausitberatung.sfgpetclinic.model;
+
+public class Vet extends Person {
+
+}
