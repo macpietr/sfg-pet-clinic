dca55c6c4693db9c25d26484c839209173b234ee
Maciej Pietruszewski <mac.pietruszewski@gmail.com>
2020-12-03 18:05:09 +0100

    add bootsrap. Closes #14

--- pet-clinic-data/src/main/java/com/matthausitberatung/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/matthausitberatung/sfgpetclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.matthausitberatung.sfgpetclinic.services.map;
 
 import com.matthausitberatung.sfgpetclinic.model.Pet;
-import com.matthausitberatung.sfgpetclinic.services.CrudService;
+import com.matthausitberatung.sfgpetclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
     public Set<Pet> findAll() {
--- pet-clinic-data/src/main/java/com/matthausitberatung/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/matthausitberatung/sfgpetclinic/services/map/VetServiceMap.java
@@ -2,10 +2,11 @@ package com.matthausitberatung.sfgpetclinic.services.map;
 
 import com.matthausitberatung.sfgpetclinic.model.Vet;
 import com.matthausitberatung.sfgpetclinic.services.CrudService;
+import com.matthausitberatung.sfgpetclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements VetService {
 
     @Override
     public Set<Vet> findAll() {
