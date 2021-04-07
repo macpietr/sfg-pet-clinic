1d2a87c6998f8ef49c3447f0eab249f4a944810f
macpietr <(Ferrari575)>
2021-04-07 23:26:57 +0200

    add PetTypeService closes #29

---
+++ pet-clinic-data/src/main/java/com/matthausitberatung/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.matthausitberatung.sfgpetclinic.services;
+
+import com.matthausitberatung.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
